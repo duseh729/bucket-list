36d081a6b68dac20fa778490cb388083cfc96fa9
Doyeon <duseh729@naver.com>
2022-12-25 23:09:44 +0900

    click-event-build

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // 클릭하면 done 클래스가 추가되도록 설계해보자
+const stamp = document.querySelectorAll(".bucket");
+console.log(stamp);
+// prettier-ignore
+stamp.forEach((element) =>element.addEventListener("click", function (e) {
+    e.target.classList.toggle("done");
+  })
+);

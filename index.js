ae48904d370b99c245dd445caee807eab02a2129
DoYeon Ahn <duseh729@naver.com>
2022-12-26 10:21:45 +0900

    localStorage-store-build

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 // 클릭하면 done 클래스가 추가되도록 설계해보자
 const stamp = document.querySelectorAll(".bucket");
 console.log(stamp);
+
+stamp.forEach(function (e) {
+  if (localStorage.getItem(e.id)) {
+    e.classList.add("done");
+  }
+});
 // prettier-ignore
 stamp.forEach((element) =>element.addEventListener("click", function (e) {
-    e.target.classList.toggle("done");
+    key=element.id
+    console.log(key)
+    if (localStorage.getItem(key)){
+      element.classList.add('done')
+    }else{
+      localStorage.setItem(element.id, 'ok');
+      element.classList.add('done')
+    }
   })
 );
